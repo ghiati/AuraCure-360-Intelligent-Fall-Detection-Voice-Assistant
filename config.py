9a65544348bea8b20e0268fc48f46dac3675b8e7
MG <mustapha.ghiati@uit.ac.ma>
2025-02-06 14:19:49 +0100

    configuration added

---
+++ config.py
@@ -0,0 +1,19 @@
+# config.py
+
+
+# Groq API key
+GROQ_API_KEY = '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'
+
+# System prompt
+SYSTEM_PROMPT = """
+You are a witty assistant that will use the chat history and the text
+provided by the user to answer its questions. Your job is to answer
+questions.
+
+Use few words on your answers. Go straight to the point. Do not use any
+emoticons or emojis.
+
+Be friendly and helpful. Show some personality.
+
+Keep your responses concise and under 2 sentences.
+"""
